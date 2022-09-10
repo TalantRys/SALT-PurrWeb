de6c16c05d3f064e7e7559f3b06514a1e3a84a2a
TalantRys <78742311+TalantRys@users.noreply.github.com>
2022-09-11 03:28:52 +0600

    countUp.js и шапка после scroll, валидация сайта
    Добавлен плагин countUp.js. с помощью которого удалось пересчитать числа из main-stats с нуля до индивидуального значения, когда произошло событие scroll.
    Шапка становится фиксированной после прокрутки секции main-preview.
    Сайт был проверен на валидаторе, были исправлены несколько маленьких ошибок.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // init Swiper
 var swiper = new Swiper('.swiper-box', {
-  // configure Swiper to use modules
+  // configure Swiper to use js/s
   direction: 'vertical',
   slidesPerView: 1,
   autoplay: {
@@ -12,11 +12,11 @@ var swiper = new Swiper('.swiper-box', {
     el: '.swiper-pagination',
     clickable: true,
   },
-  breakpoints:{
-    1:{
+  breakpoints: {
+    1: {
       direction: 'horizontal',
     },
-    575:{
+    575: {
       direction: 'vertical',
     }
   }
@@ -24,35 +24,98 @@ var swiper = new Swiper('.swiper-box', {
 
 // Magnific PopUp
 
-$(document).ready(function() {
-	$('.spotlight__video').magnificPopup({
-		disableOn: 700,
+$(document).ready(function () {
+  $('.spotlight__video').magnificPopup({
+    disableOn: 700,
     items: {
       src: 'https://www.youtube.com/watch?v=ltMSrSis9ww'
     },
-		type: 'iframe',
-		mainClass: 'mfp-fade',
-		removalDelay: 160,
-		preloader: false,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
 
-		fixedContentPos: true
-	});
+    fixedContentPos: true
+  });
+});
+
+// ШАПКА ФИКСИРОВАНА ПРИ СКРОЛЛЕ
+const header = document.querySelector('.header');
+const preview = document.querySelector('.main-preview');
+const headerHeight = header.offsetHeight;
+const previewHeight = preview.offsetHeight;
+
+console.log(headerHeight, previewHeight);
+window.addEventListener('scroll', function () {
+  let scrollDistance = window.pageYOffset;
+  if (scrollDistance >= previewHeight) {
+    header.classList.add('header_fixed');
+  } else {
+    header.classList.remove('header_fixed');
+  }
 });
 
 // БУРГЕР
 const burger = document.querySelector('header .header__burger');
 const burgerBody = document.querySelector('body');
 const list = document.querySelector('header .header__list');
-burger.addEventListener('click', function(){
-    burgerBody.classList.toggle('lock');
-    burger.classList.toggle('close');
-    list.classList.toggle('burger-active');
+burger.addEventListener('click', function () {
+  burgerBody.classList.toggle('lock');
+  burger.classList.toggle('close');
+  list.classList.toggle('burger-active');
 });
 const listLink = document.querySelectorAll('.header__list .header__link');
-listLink.forEach((link)=> {
-    link.addEventListener('click', function () {
-        burgerBody.classList.remove('lock');
-        burger.classList.remove('close');
-        list.classList.remove('burger-active');
-    });
+listLink.forEach((link) => {
+  link.addEventListener('click', function () {
+    burgerBody.classList.remove('lock');
+    burger.classList.remove('close');
+    list.classList.remove('burger-active');
+  });
 });
+
+// COUNT-UP.JS
+import { CountUp } from "./countup.js/dist/countUp.min.js";
+
+window.onload = function () {
+  const statsNumber1 = document.querySelector('#stats-value-1'),
+    statsNumber2 = document.querySelector('#stats-value-2'),
+    statsNumber3 = document.querySelector('#stats-value-3')
+
+  const options1 = {
+    decimal: ',',
+    decimalPlaces: 3,
+    duration: 1,
+  };
+  const options2 = {
+    decimalPlaces: 1,
+    duration: 1,
+    prefix: '$',
+    suffix: 'bn',
+  };
+  let demo1 = new CountUp(statsNumber1, 126.300, options1);
+  let demo2 = new CountUp(statsNumber2, 20.1, options2);
+  let demo3 = new CountUp(statsNumber3, 45.511, options1);
+
+  window.addEventListener('scroll', function () {
+    let scrollDistance = window.pageYOffset;
+    console.log(scrollDistance)
+    if (scrollDistance >= previewHeight-500) {
+      if (!demo1.error) {
+        demo1.start();
+      } else {
+        console.error(demo1.error);
+      }
+      if (!demo2.error) {
+        demo2.start();
+      } else {
+        console.error(demo2.error);
+      }
+      if (!demo3.error) {
+        demo3.start();
+      } else {
+        console.error(demo3.error);
+      }
+    }
+  });
+  
+}
